e6a2790f3d31c31a169ea316a97c3940661a96bd
zhenghongwei <zhenghongwei@airm2m.com>
2021-12-28 16:49:50 +0800

    add:用户打开工程数据接收及处理逻辑提交

--- src/webview/openProjectWebview.ts
+++ src/webview/openProjectWebview.ts
@@ -5,12 +5,11 @@ import path = require('path');
 import { ProjectConfigOperation } from '../project/ProjectHandle';
 import { ProjectJsonParse } from '../project/projectConfigParse';
 import { PluginJsonParse } from '../plugConfigParse';
-import { CreateProject } from '../project/createProject';
-
+import {checkSameProjectExistStatusForPluginConfig} from '../project/projectApi';
 let pluginVariablesInit = new PluginVariablesInit();
 let projectConfigOperation = new ProjectConfigOperation();
 let pluginJsonParse = new PluginJsonParse();
-
+let projectJsonParse = new ProjectJsonParse();
 export class OpenProjectManage {
     constructor() {
 
@@ -37,7 +36,6 @@ export class OpenProjectManage {
                 }
             );
         }
-
         // 获取webview界面
         this.openopenProjectPanel.webview.html = this.getProjectWebviewContent();
 
@@ -102,16 +100,9 @@ export class OpenProjectManage {
         switch (message.command) {
             // 用户导入工程信息接收
             case 'importProject':
-                switch(message.text){
-                    case "pure":
-                        break;
-                    case "example":
-                        break;
-                    case "ndk":
-                        break;
-                    case "ui":
-                        break;
-                }
+                // 处理导入工程传过来的路径数据
+                this.openProjectReceiveDataHandle(message);
+                break;
             case 'Alert':
                 vscode.window.showErrorMessage(message.text['msg'],{modal: true});
                 break;
@@ -182,8 +173,60 @@ export class OpenProjectManage {
                         break;
                 }
                 break;
+            case 'cancelProject':
+                openProjectPanel.dispose();
+                break;
         }
     }
 
+    openProjectReceiveDataHandle(message:any){
+        let openProjectMessage = {
+            openProjectProjectType:message.text.type,
+            openProjectName: message.text.data.projectName,
+            openProjectPath: message.text.data.projectPath,
+            openProjectModuleModel: message.text.data.moduleModel,
+            openProjectLibPath: message.text.data.libPath,
+            openProjectCorePath: message.text.data.corePath,
+            openProjectExample: message.text.data.example,
+        };
+        const openProjectCheckState:boolean = this.openProjectCheck(message);
+        if (!openProjectCheckState) { //新建工程必要条件检查未通过
+            return false;
+        }
+        // 将打开工程名称和路径信息插入插件配置文件
+        const nameIndex:number = openProjectMessage.openProjectPath.lastIndexOf("\\");
+        const projectParentPath:string = openProjectMessage.openProjectPath.substring(0,nameIndex);
+        const projectObj = {
+            projectPath:projectParentPath,
+            projectName:openProjectMessage.openProjectName,
+        };
+        pluginJsonParse.pushPluginConfigProject(projectObj);
+        // const appFile:string = getFileForDir(openProjectMessage.openProjectPath);         //appfile采用用户自己的appfile
+        // projectJsonParse.setProjectConfigAppFile(appFile,openProjectMessage.openProjectPath);
+        const projectConfigVersion:string = projectJsonParse.getprojectConfigInitVersion();
+        projectJsonParse.setProjectConfigProjectType(openProjectMessage.openProjectProjectType,openProjectMessage.openProjectPath);
+        projectJsonParse.setProjectConfigVersion(projectConfigVersion,openProjectMessage.openProjectPath);
+        projectJsonParse.setProjectConfigCorePath(openProjectMessage.openProjectCorePath,openProjectMessage.openProjectPath);
+        projectJsonParse.setProjectConfigLibPath(openProjectMessage.openProjectLibPath,openProjectMessage.openProjectPath);
+        projectJsonParse.setProjectConfigModuleModel(openProjectMessage.openProjectModuleModel,openProjectMessage.openProjectPath);
+        vscode.window.showInformationMessage(`工程${openProjectMessage.openProjectName}已导入成功，请切换到用户工程查看`,{modal: true});
+    }
 
+    // 打开工程必要条件检查
+    openProjectCheck(message:any){
+        const projectName:any = message.text.projectName;
+        const projectPath:any = message.text.projectPath;
+        const projectModule:any =message.text.moduleModel;
+        if (projectName === "" || projectPath  === "" || projectModule === "") {
+            vscode.window.showErrorMessage("打开工程webview数据接收失败!!",{modal: true});
+            return false;
+        }
+        const sameProjectExistStatus:boolean = checkSameProjectExistStatusForPluginConfig(projectName);
+        // 检查用户历史工程看工程名称是否重复
+        if (sameProjectExistStatus) {
+            vscode.window.showErrorMessage("该工程已被建立，不能重新建立" ,{modal: true});
+            return false;
+        }
+        return true;
+    }
 }
