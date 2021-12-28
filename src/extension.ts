6bc212f60a20022f198e9115eb42862862b9858f
zhenghongwei <zhenghongwei@airm2m.com>
2021-12-28 16:52:41 +0800

    delete:删除冗余命令及数据

--- src/extension.ts
+++ src/extension.ts
@@ -84,8 +84,6 @@ const runMode: 'external' | 'server' | 'inline' = 'inline';
 export function activate(context: vscode.ExtensionContext) {
 	// 注册新建工程命令,当点击用户历史工程标题区域新建工程按钮时触发
 	context.subscriptions.push(vscode.commands.registerCommand('luatide-history-project.createProject',async ()=> projectManage.projectManage(context)));
-	// 注册打开工程命令,当点击用户历史工程标题区域打开工程按钮时触发
-	context.subscriptions.push(vscode.commands.registerCommand('luatide-history-project.openProject',openProject));
 	// 注册运行工程命令,当点击活动工程标题区域运行工程按钮时触发
 	context.subscriptions.push(vscode.commands.registerCommand('luatide-activity-project.runProject',runProject));
 	// 注册调试工程命令,当点击活动工程标题区域调试工程按钮时触发
@@ -99,9 +97,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// 注册删除工程文件命令,当点击活动工程内部区域删除工程文件夹或文件按钮时触发
 	context.subscriptions.push(vscode.commands.registerCommand('luatide-history-project.Project.delete',projectSeletcedDelete));
 	// 注册点击home主页命令,当点击历史工程标题区域主页按钮时触发
-	context.subscriptions.push(vscode.commands.registerCommand('luatide-history-project.Home',async ()=> homeManage));
-	// 注册点击登录命令,当点击home主页内登录按钮时触发
-	context.subscriptions.push(vscode.commands.registerCommand('luatide-plugin.Login',async ()=> homeManage));
 	context.subscriptions.push(vscode.commands.registerCommand('luatide-history-project.Home',async ()=> homeManage.homeManage(context)));
 	// 注册点击活动工程配置命令,当点击配置活动工程时触发
 	context.subscriptions.push(vscode.commands.registerCommand('luatide-activity-project.configOperation',projectConfigOperation.projectConfigOperation));
--- src/project/openProject.ts
+++ src/project/openProject.ts
@@ -67,6 +67,8 @@ export class OpenProject {
 					await vscode.window.showErrorMessage("该项目未配置工程，是否配置？", { modal: true }, "是", "否").then(result => {
 						if (result === '是') {
                             projectJsonParse.generateProjectJson(importProjectPath);
+                            const appFile:string = getFileForDir(importProjectPath);
+                            projectJsonParse.pushProjectConfigAppFile(appFile,importProjectPath);
                             return importProjectPath;
 						}
 						else if (result === '否') {
@@ -81,52 +83,5 @@ export class OpenProject {
             };
         });
         return importProjectPath;
-    }
-
-    openProjectReceiveDataHandle(message:any){
-        let openProjectMessage = {
-            openProjectName: message.text['project_name'],
-            openProjectPath: message.text['project_path'],
-            openProjectModuleModel: message.text['module_type'],
-            openProjectLibPath: message.text['lib_path'],
-            openProjectCorePath: message.text['core_path'],
-            openProjectExample: message.text['project_example'],
-        };
-        const openProjectCheckState:boolean = this.openProjectCheck(message);
-        if (!openProjectCheckState) { //新建工程必要条件检查未通过
-            return false;
-        }
-        // 将打开工程名称和路径信息插入插件配置文件
-        const projectObj = {
-            projectPath:openProjectMessage.openProjectPath,
-            projectName:openProjectMessage.openProjectName,
-        };
-        pluginJsonParse.pushPluginConfigProject(projectObj);
-        projectJsonParse.generateProjectJson(openProjectMessage.openProjectPath); //初始化写入工程配置文件
-        const appFile:string = getFileForDir(openProjectMessage.openProjectPath);
-        projectJsonParse.setProjectConfigAppFile(appFile);
-        const projectConfigVersion:string = projectJsonParse.getprojectConfigInitVersion();
-        projectJsonParse.setProjectConfigVersion(projectConfigVersion);
-        projectJsonParse.setProjectConfigCorePath(openProjectMessage.openProjectPath);
-        projectJsonParse.setProjectConfigLibPath(openProjectMessage.openProjectLibPath);
-        projectJsonParse.setProjectConfigModuleModel(openProjectMessage.openProjectModuleModel);
-        vscode.window.showInformationMessage(`工程${openProjectMessage.openProjectName}已导入成功，请切换到用户工程查看`,{modal: true});
-    }
-
-    // 打开工程必要条件检查
-    openProjectCheck(message:any){
-        const projectName:any = message.text['project_name'];
-        const projectPath:any = message.text['project_path'];
-        const projectModule:any = message.text['module_type'];
-        if (projectName === "" || projectPath  === "" || projectModule === "") {
-            vscode.window.showErrorMessage("打开工程webview数据接收失败!!",{modal: true});
-        }
-        const sameProjectExistStatus:boolean = checkSameProjectExistStatusForPluginConfig(projectName);
-        // 检查用户历史工程看工程名称是否重复
-        if (sameProjectExistStatus) {
-            vscode.window.showErrorMessage("该工程已被建立，不能重新建立" ,{modal: true});
-            return false;
-        }
-        return true;
     }
 }
