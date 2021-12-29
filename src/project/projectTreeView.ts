799b60eac7cf60690e0b923e24918ff475764a05
zhenghongwei <zhenghongwei@airm2m.com>
2021-12-29 13:54:42 +0800

    add:用户工程treeview刷新功能添加，工程列表刷新逻辑修改

--- src/project/projectTreeView.ts
+++ src/project/projectTreeView.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
-import * as path from 'path';
 import { PluginJsonParse } from '../plugConfigParse';
 import { PluginConfigInit, PluginVariablesInit } from '../config';
 import { ProjectJsonParse } from './projectConfigParse';
@@ -10,61 +9,35 @@ let pluginJsonParse = new PluginJsonParse();
 let pluginVariablesInit = new PluginVariablesInit();
 let projectJsonParse = new ProjectJsonParse();
 export class NodeDependenciesProvider implements vscode.TreeDataProvider<Dependency> {
-  constructor(private workspaceRoot: string) {}
+  constructor() { }
   private _onDidChangeTreeData: vscode.EventEmitter<Dependency | undefined | null | void> = new vscode.EventEmitter<Dependency | undefined | null | void>();
   readonly onDidChangeTreeData: vscode.Event<Dependency | undefined | null | void> = this._onDidChangeTreeData.event;
-  
+  public rootloadUrlArray = new Array();
+
   getTreeItem(element: Dependency): vscode.TreeItem {
     return element;
   }
 
   getChildren(element?: Dependency): Thenable<Dependency[]> {
-    if (!this.workspaceRoot) {
-      vscode.window.showInformationMessage('No dependency in empty workspace');
-      return Promise.resolve([]);
-    }
-
-    if (element) {
-      return Promise.resolve(
-        this.getUserProjectInPackageJson(
-          path.join(this.workspaceRoot, 'node_modules', element.label, 'package.json')
-        )
-      );
-    } else {
-      const packageJsonPath = pluginVariablesInit.getPluginConfigPath();
-      if (this.pathExists(packageJsonPath)) {
-        return Promise.resolve(this.getUserProjectInPackageJson(packageJsonPath));
-      } else {
-        vscode.window.showInformationMessage('Workspace has no package.json');
-        return Promise.resolve([]);
-      }
-    }
+    this.rootloadUrlArray = [];
+    let userProjectAbsulutePathList: any = pluginJsonParse.getPluginConfigUserProjectAbsolutePathList();
+    this.generateTreeItem(userProjectAbsulutePathList);
+    return Promise.resolve(this.rootloadUrlArray);
   }
 
-//   解析用户工程配置文件并在用户工程区域做展示
-private getUserProjectInPackageJson(pluginJsonPath: string): Dependency[] {
-    const toDep = (projectName: string, projectPath: string): Dependency => {
-        return new Dependency(projectName,projectPath, vscode.TreeItemCollapsibleState.None);
-    };
-    const userProjectAbsulutePathList = pluginJsonParse.getPluginConfigUserProjectAbsolutePathList();
-    
-    const deps = userProjectAbsulutePathList?Object.keys(userProjectAbsulutePathList).map(
-      dep => toDep(userProjectAbsulutePathList[dep].split('\\').pop(),userProjectAbsulutePathList[dep]))
-      :[];
-      // for (let index = 0; index < userProjectAbsulutePathList.length; index++) {
-      //     const projectPath = userProjectAbsulutePathList[index];
-      //     const projectName:any = projectPath.split('\\')[-1];
-      //     toDep(projectName,projectPath);
-      // }
-    return deps;
-}
+  generateTreeItem(userProjectAbsulutePathList: any) {
 
-// 构造用户工程显示json
-// private generateUserProjectShow(){
-//   const projectJson:any = '';
-//   const projectJsonObj = JSON.parse(projectJson);
-//   const projectAppFile = projectJsonObj.Appf
-// }
+    for (let index = 0; index < userProjectAbsulutePathList.length; index++) {
+      const projectAbsolutePath = userProjectAbsulutePathList[index];
+      if (fs.existsSync(projectAbsolutePath)) {
+        const nameIndex = projectAbsolutePath.lastIndexOf('\\');
+        const projectName: any = projectAbsolutePath.substring(nameIndex + 1);
+        const projectPath = projectAbsolutePath.substring(0, nameIndex);
+        this.rootloadUrlArray.push(new Dependency(projectName, projectPath, vscode.TreeItemCollapsibleState.None));
+      }
+    }
+
+  }
 
   private pathExists(p: string): boolean {
     try {
@@ -75,26 +48,19 @@ private getUserProjectInPackageJson(pluginJsonPath: string): Dependency[] {
     return true;
   }
 
-
-
   refresh(): void {
     this._onDidChangeTreeData.fire();
   }
 }
 
-class Dependency extends vscode.TreeItem {
+export class Dependency extends vscode.TreeItem {
   constructor(
     public readonly label: string,
     private path: string,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState
   ) {
-    super(label, collapsibleState);  
-    this.tooltip = path;
-    // this.description = "111"; 
+    super(label, collapsibleState);
+    this.tooltip = this.path;
   }
-
-  // iconPath = {
-  //   light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
-  //   dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
-  // };
 }
+
