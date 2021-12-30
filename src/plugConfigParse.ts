1507fe7087563100a5f7d5565d8431c4593ba0bf
zhenghongwei <zhenghongwei@airm2m.com>
2021-12-30 17:47:26 +0800

    add:新增用户工程兼容功能

--- src/plugConfigParse.ts
+++ src/plugConfigParse.ts
@@ -104,41 +104,103 @@ let pluginVariablesInit = new PluginVariablesInit();
         this.refreshPlugintJson(pluginJsonObj);
     }
 
-    // 插件配置文件兼容
-    pluginConfigCompatible(){
-        // plugin版本2.0以下兼容
-        const pluginConfigPath:string = pluginVariablesInit.getPluginConfigPath();
-        const pluginJson:string = fs.readFileSync(pluginConfigPath,'utf-8');
-        const pluginJsonObj = JSON.parse(pluginJson);
-        let luatideWorkspaceJson:any = {
-            version:'',
-            projectList:[],
-            activeProject:'',
-        };
-        if (Number(pluginJsonObj.version) < 2.0) {
-            // 活动工程兼容
-            const activityProject:string = pluginJsonObj['active_workspace'];
-            luatideWorkspaceJson.activeProject  = activityProject;
-            // 用户工程数据兼容
-            for (let index = 0; index < pluginJsonObj.data.length; index++) {
-                const projectObjType = pluginJsonObj.data[index];
-                if (projectObjType.type==='user') {
-                    for (let i = 0; i < projectObjType.projects.length; i++) {
-                        const userProjectObj = projectObjType.projects[i];
-                        const projectName = userProjectObj['project_name'];
-                        const projectPath = userProjectObj['project_path'];
-                        let projectObjNew = {
-                            projectName:projectName,
-                            projectPath:projectPath
-                        };
-                        luatideWorkspaceJson.projectList.push(projectObjNew);
-                    }
-                } 
-            }
-            // 插件版本兼容
-            luatideWorkspaceJson.version='2.0';
-            const pluginConfigJsonNew = JSON.stringify(luatideWorkspaceJson);
-            fs.writeFileSync(pluginConfigPath,pluginConfigJsonNew);
-        }
-    }
-}
+     // 插件配置文件兼容
+     pluginConfigCompatible() {
+         // plugin版本2.0以下兼容
+         const pluginConfigPath: string = pluginVariablesInit.getPluginConfigPath();
+         const pluginJson: string = fs.readFileSync(pluginConfigPath, 'utf-8');
+         const pluginJsonObj = JSON.parse(pluginJson);
+         let luatideWorkspaceJson: any = {
+             version: '',
+             projectList: [],
+             activeProject: '',
+         };
+         if (Number(pluginJsonObj.version) < 2.0) {
+             // 活动工程兼容
+             const activityProject: string = pluginJsonObj['active_workspace'];
+             luatideWorkspaceJson.activeProject = activityProject;
+             // 用户工程数据兼容
+             for (let index = 0; index < pluginJsonObj.data.length; index++) {
+                 const projectObjType = pluginJsonObj.data[index];
+                 if (projectObjType.type === 'user') {
+                     for (let i = 0; i < projectObjType.projects.length; i++) {
+                         const userProjectObj = projectObjType.projects[i];
+                         const projectName = userProjectObj['project_name'];
+                         const projectPath = userProjectObj['project_path'];
+                         let projectObjNew = {
+                             projectName: projectName,
+                             projectPath: projectPath
+                         };
+                         luatideWorkspaceJson.projectList.push(projectObjNew);
+                     }
+                 }
+             }
+             // 插件版本兼容
+             luatideWorkspaceJson.version = '2.0';
+             const pluginConfigJsonNew = JSON.stringify(luatideWorkspaceJson,null,"\t");
+             fs.writeFileSync(pluginConfigPath, pluginConfigJsonNew);
+         }
+     }
+
+     // 工程配置文件兼容
+     projectConfigCompatible(projectPath: string) {
+         const projectConfigPath: string = path.join(projectPath, 'luatide_project.json');
+         if (!fs.existsSync(projectConfigPath)) {
+             return;
+         }
+         const projectOldJson: string = fs.readFileSync(projectConfigPath, 'utf-8');
+         const projectOldJsonObj = JSON.parse(projectOldJson);
+         let luatideProjectNewJson: any = {
+             version: '',
+             projectType: 'pure',
+             corePath: '',
+             libPath: '',
+             modulePath: '',
+             appFile: [],
+             modulePort: '',
+         };
+         if (Number(projectOldJsonObj.version) < 2.0) {
+             // 用户core路径兼容
+             const corePath: string = projectOldJsonObj['corefile_path'];
+             luatideProjectNewJson.corePath = corePath;
+             // 用户lib路径兼容
+             let libPath: string = projectOldJsonObj['lib_path'];
+             if (libPath === '' && projectOldJsonObj['module_model'] !== 'Air10X') {
+                 libPath = pluginVariablesInit.getAir72XDefaultLatestLibPath();
+             }
+             luatideProjectNewJson.libPath = libPath;
+             // 用户模块型号判断
+             const moduleModelOld: string = projectOldJsonObj['module_model'];
+             switch (moduleModelOld) {
+                 case 'Air72XUX/Air82XUX':
+                     luatideProjectNewJson.moduleModel = 'air72XUX/air82XUX';
+                     break;
+                 case 'Air72XCX':
+                     luatideProjectNewJson.moduleModel = 'air72XCX';
+                     break;
+                 case 'Air10X':
+                     luatideProjectNewJson.moduleModel = 'air10X';
+                     break;
+                 case 'Simulator':
+                     luatideProjectNewJson.moduleModel = 'simulator';
+                     break;
+             }
+             // 用户appFile文件兼容
+             const appFileOld: string[] = projectOldJsonObj['app_file'];
+             for (let i = 0; i < appFileOld.length; i++) {
+                 const appFileOldPath: string = appFileOld[i];
+                 if (appFileOldPath.indexOf(projectPath) !== -1) {
+                     luatideProjectNewJson.appFile.push(appFileOldPath);
+                 }
+             }
+             // 用户工程版本兼容
+             luatideProjectNewJson.version = '2.0';
+             //用户modulePort兼容
+             if (projectOldJsonObj['module_port']) {
+                 luatideProjectNewJson.modulePort = projectOldJsonObj['module_port'];
+             }
+             const projectConfigJsonNew = JSON.stringify(luatideProjectNewJson,null,"\t");
+             fs.writeFileSync(projectConfigPath, projectConfigJsonNew,);
+         }
+     }
+ }
