fcb14aac837a8af727ba2d074239a3feadf09606
chenxiahuaxu <863025212@qq.com>
2022-01-06 15:34:16 +0800

    编译错误，导入有问题

--- src/webview/openProjectWebview.ts
+++ src/webview/openProjectWebview.ts
@@ -6,6 +6,7 @@ import { ProjectConfigOperation } from '../project/ProjectHandle';
 import { ProjectJsonParse } from '../project/projectConfigParse';
 import { PluginJsonParse } from '../plugConfigParse';
 import {checkSameProjectExistStatusForPluginConfig} from '../project/projectApi';
+const {fetch} = require('node-fetch');
 let pluginVariablesInit = new PluginVariablesInit();
 let projectConfigOperation = new ProjectConfigOperation();
 let pluginJsonParse = new PluginJsonParse();
