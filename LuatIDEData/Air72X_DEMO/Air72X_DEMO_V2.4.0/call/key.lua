d9594eeb3f51fdafe0cac95bd88daef9c8614a52
zhenghongwei <zhenghongwei@airm2m.com>
2022-02-09 17:49:27 +0800

    delete:删除自带的data数据，旧有的临时处理方案已弃用，现采用服务器实时拉取更新的方式

--- LuatIDEData/Air72X_DEMO/Air72X_DEMO_V2.4.0/call/key.lua
+++
@@ -1,5 +0,0 @@
-
-module(...,package.seeall)
-require"powerKey"
-
-powerKey.setup(3000,function() end, function() sys.publish("POWER_KEY_IND") end)
--- LuatIDEData/Air72X_DEMO/Air72X_DEMO_V2.4.0/nvm/readme.txt
+++
@@ -1,2 +0,0 @@
-参考：http://ask.openluat.com/article/927
-加深对nvm的理解
