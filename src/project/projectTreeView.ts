2587023203c03026f6f3a3f964969d3173bbece3
chenxiahuaxu <863025212@qq.com>
2022-02-18 20:01:41 +0800

    格式化代码

--- src/project/projectTreeView.ts
+++ src/project/projectTreeView.ts
@@ -9,61 +9,61 @@ let pluginJsonParse = new PluginJsonParse();
 // let pluginVariablesInit = new PluginVariablesInit();
 // let projectJsonParse = new ProjectJsonParse();
 export class HistoryProjectDataProvider implements vscode.TreeDataProvider<HistoryProjectTreeItem> {
-  constructor() { }
-  private _onDidChangeTreeData: vscode.EventEmitter<HistoryProjectTreeItem | undefined | null | void> = new vscode.EventEmitter<HistoryProjectTreeItem | undefined | null | void>();
-  readonly onDidChangeTreeData: vscode.Event<HistoryProjectTreeItem | undefined | null | void> = this._onDidChangeTreeData.event;
-  public rootloadUrlArray = new Array();
+	constructor() { }
+	private _onDidChangeTreeData: vscode.EventEmitter<HistoryProjectTreeItem | undefined | null | void> = new vscode.EventEmitter<HistoryProjectTreeItem | undefined | null | void>();
+	readonly onDidChangeTreeData: vscode.Event<HistoryProjectTreeItem | undefined | null | void> = this._onDidChangeTreeData.event;
+	public rootloadUrlArray = new Array();
 
-  getTreeItem(element: HistoryProjectTreeItem): vscode.TreeItem {
-    return element;
-  }
+	getTreeItem(element: HistoryProjectTreeItem): vscode.TreeItem {
+		return element;
+	}
 
-  getChildren(element?: HistoryProjectTreeItem): Thenable<HistoryProjectTreeItem[]> {
-    this.rootloadUrlArray = [];
-    let userProjectAbsulutePathList: any = pluginJsonParse.getPluginConfigUserProjectAbsolutePathList();
-    this.generateTreeItem(userProjectAbsulutePathList);
-    return Promise.resolve(this.rootloadUrlArray);
-  }
+	getChildren(element?: HistoryProjectTreeItem): Thenable<HistoryProjectTreeItem[]> {
+		this.rootloadUrlArray = [];
+		let userProjectAbsulutePathList: any = pluginJsonParse.getPluginConfigUserProjectAbsolutePathList();
+		this.generateTreeItem(userProjectAbsulutePathList);
+		return Promise.resolve(this.rootloadUrlArray);
+	}
 
-  generateTreeItem(userProjectAbsulutePathList: any) {
+	generateTreeItem(userProjectAbsulutePathList: any) {
 
-    for (let index = 0; index < userProjectAbsulutePathList.length; index++) {
-      const projectAbsolutePath = userProjectAbsulutePathList[index];
-      const nameIndex = projectAbsolutePath.lastIndexOf('\\');
-      const projectName: any = projectAbsolutePath.substring(nameIndex + 1);
-      const projectPath = projectAbsolutePath.substring(0, nameIndex);
-      if (fs.existsSync(projectAbsolutePath)) {
-        this.rootloadUrlArray.push(new HistoryProjectTreeItem(projectName, projectPath, vscode.TreeItemCollapsibleState.None));
-      }
-      else{
-        vscode.window.showInformationMessage(`工程${projectName}路径已发生改变,已从您的用户工程列表中移除`);
-        pluginJsonParse.popPluginConfigProject(projectName);
-      }
-    }
-  }
+		for (let index = 0; index < userProjectAbsulutePathList.length; index++) {
+			const projectAbsolutePath = userProjectAbsulutePathList[index];
+			const nameIndex = projectAbsolutePath.lastIndexOf('\\');
+			const projectName: any = projectAbsolutePath.substring(nameIndex + 1);
+			const projectPath = projectAbsolutePath.substring(0, nameIndex);
+			if (fs.existsSync(projectAbsolutePath)) {
+				this.rootloadUrlArray.push(new HistoryProjectTreeItem(projectName, projectPath, vscode.TreeItemCollapsibleState.None));
+			}
+			else {
+				vscode.window.showInformationMessage(`工程${projectName}路径已发生改变,已从您的用户工程列表中移除`);
+				pluginJsonParse.popPluginConfigProject(projectName);
+			}
+		}
+	}
 
-  // private pathExists(p: string): boolean {
-  //   try {
-  //     fs.accessSync(p);
-  //   } catch (err) {
-  //     return false;
-  //   }
-  //   return true;
-  // }
+	// private pathExists(p: string): boolean {
+	//   try {
+	//     fs.accessSync(p);
+	//   } catch (err) {
+	//     return false;
+	//   }
+	//   return true;
+	// }
 
-  refresh(): void {
-    this._onDidChangeTreeData.fire();
-  }
+	refresh(): void {
+		this._onDidChangeTreeData.fire();
+	}
 }
 
 export class HistoryProjectTreeItem extends vscode.TreeItem {
-  constructor(
-    public readonly label: string,
-    private path: string,
-    public readonly collapsibleState: vscode.TreeItemCollapsibleState
-  ) {
-    super(label, collapsibleState);
-    this.tooltip = this.path;
-  }
+	constructor(
+		public readonly label: string,
+		private path: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState
+	) {
+		super(label, collapsibleState);
+		this.tooltip = this.path;
+	}
 }
 
