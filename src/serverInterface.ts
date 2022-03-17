d3587c6edf4da7ab08bc04b09d24b49c2bd67d8d
chenxiahuaxu <863025212@qq.com>
2022-03-17 15:25:07 +0800

    fix:close时需要关掉中端

--- src/serverInterface.ts
+++ src/serverInterface.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: czm
  * @Date: 2022-03-16 11:32:34
- * @LastEditTime: 2022-03-17 11:01:17
+ * @LastEditTime: 2022-03-17 15:09:26
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \luatide\src\serverInterface.ts
@@ -119,6 +119,7 @@ export async function open(mockhand:any) {
 
 export async function close() {
     gSocketHandle?.destroy();
+    vscode.commands.executeCommand("workbench.action.terminal.kill");
     return true;
 }
 
