d94419675eccba8da5b26293a67dcc1de2dcc2d5
zhenghongwei <zhenghongwei@airm2m.com>
2022-05-21 14:19:58 +0800

    update:活动工程treeview新设计功能代码提交

--- src/project/activityProjectTreeView.ts
+++ src/project/activityProjectTreeView.ts
@@ -3,7 +3,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 // import { ProjectJsonParse } from './projectConfigParse';
 import { getPluginConfigActivityProject, setPluginConfigActivityProject } from '../plugConfigParse';
-import { getProjectConfigAppFile } from './projectConfigParse';
+import { getProjectConfigAppFile, getProjectConfigLibPath } from './projectConfigParse';
+import { getactiveProjectConfigDesc, getApiDesc, getConnectPort, getDistinguishMark, getHardwaveDesc, getHelp, getProductionFileDesc, getSimulatorDesc, getUiDesignDesc } from './activityProjectConfig';
 
 // let pluginJsonParse = new PluginJsonParse();
 // let projectJsonParse = new ProjectJsonParse();
@@ -22,68 +23,101 @@ export class ActivityTreeDataProvider implements vscode.TreeDataProvider<Activit
   }
 
   getChildren(element?: ActivityTreeItem): Thenable<ActivityTreeItem[]> {
-    var treeDir: ActivityTreeItem[] = [];
     let activityPath: string = getPluginConfigActivityProject();
+    let treeDir: ActivityTreeItem[] = [];
     if (element === undefined) {
       if (activityPath === '') {
         return Promise.resolve([]);
       }
-      else if (!fs.existsSync(path.join(activityPath,'luatide_project.json'))) {
+      else if (!fs.existsSync(path.join(activityPath, 'luatide_project.json'))) {
         setPluginConfigActivityProject('');
         return Promise.resolve([]);
       }
-      // const nameIndex: number = activityPath.lastIndexOf("\\");
-      // const activityParentPath: string = activityPath.substring(0, nameIndex);
-      // const activityProjectName: string = activityPath.substring(nameIndex + 1);
-      const activityProjectJson:any = JSON.parse(fs.readFileSync(path.join(activityPath,'luatide_project.json'),'utf-8'));
-      const activityProjectPath:string = path.dirname(activityPath);
-      const activityProjectName:string = activityProjectJson.projectName;
-      treeDir.push(new ActivityTreeItem(activityProjectName, activityProjectPath, vscode.TreeItemCollapsibleState.Expanded));
-      return Promise.resolve(treeDir);
+      treeDir = activeProjectDataInit(activityPath);
     }
     else {
-      const fileParentPath: string = element['parentPath'];
-      const filename: string = element['label'];
-      let filePath: string;
-      if (path.dirname(activityPath)===fileParentPath){
-        filePath = activityPath;
+      treeDir = childrenDataInit(element, activityPath);
+    }
+    return Promise.resolve(treeDir);
+  }
+
+}
+
+// 初始化活动工程数据
+function activeProjectDataInit(activityPath: string) {
+  const treeDir: ActivityTreeItem[] = [];
+  const activityProjectJson: any = JSON.parse(fs.readFileSync(path.join(activityPath, 'luatide_project.json'), 'utf-8'));
+  const activityProjectPath: string = path.dirname(activityPath);
+  const activityProjectName: string = activityProjectJson.projectName;
+  const libParentPath = path.dirname(activityProjectJson.libPath);
+  const libName = path.basename(activityProjectJson.libPath);
+  treeDir.push(new ActivityTreeItem(activityProjectName, activityProjectPath, vscode.TreeItemCollapsibleState.Expanded));
+  if (activityProjectJson.libPath !== "" && fs.existsSync(activityProjectJson.libPath)) {
+    treeDir.push(new ActivityTreeItem(libName, libParentPath, vscode.TreeItemCollapsibleState.Collapsed));
+  }
+  treeDir.push(new ActivityTreeItem(getactiveProjectConfigDesc(), '', vscode.TreeItemCollapsibleState.None));
+  treeDir.push(new ActivityTreeItem(getProductionFileDesc(), '', vscode.TreeItemCollapsibleState.None));
+  treeDir.push(new ActivityTreeItem(getUiDesignDesc(), '', vscode.TreeItemCollapsibleState.None));
+  treeDir.push(new ActivityTreeItem(getSimulatorDesc(), '', vscode.TreeItemCollapsibleState.None));
+  treeDir.push(new ActivityTreeItem(getConnectPort(), '', vscode.TreeItemCollapsibleState.None));
+  treeDir.push(new ActivityTreeItem(getHelp(), getDistinguishMark(), vscode.TreeItemCollapsibleState.Collapsed));
+  return treeDir;
+}
+
+// 活动工程子数据刷新
+function childrenDataInit(element: any,activityPath) {
+  const treeDir: ActivityTreeItem[] = [];
+  const fileParentPath: string = element['parentPath'];
+  const filename: string = element['label'];
+  const libPath: string = getProjectConfigLibPath(activityPath);
+  if (path.join(fileParentPath, filename) === libPath) {
+    const files = fs.readdirSync(libPath);
+    for (let i = 0; i < files.length; i++) {
+      const childrenFileName: string = files[i];
+      const childrenFilePath: string = path.join(libPath, childrenFileName);
+      if (fs.statSync(childrenFilePath).isFile()) {
+        treeDir.push(new ActivityTreeItem(childrenFileName, libPath, vscode.TreeItemCollapsibleState.None));
       }
-      else{
-        filePath = path.join(fileParentPath, filename);
+      else {
+        treeDir.unshift(new ActivityTreeItem(childrenFileName, libPath, vscode.TreeItemCollapsibleState.Collapsed));
       }
-      const files = fs.readdirSync(filePath);
-      // const activityPath: string = getPluginConfigActivityProject();
-      const appFile = getProjectConfigAppFile(activityPath);
-      if (appFile !== undefined) {
-        for (let i = 0; i < files.length; i++) {
-          const childrenFileName: string = files[i];
-          const childrenFilePath: string = path.join(filePath, childrenFileName);
-          const relativeFilePath: string = path.relative(activityPath, childrenFilePath);
-          if (appFile.indexOf(relativeFilePath) !== -1) {
-            if (fs.statSync(childrenFilePath).isFile()) {
-                treeDir.push(new ActivityTreeItem(childrenFileName, filePath, vscode.TreeItemCollapsibleState.None));
-            }
-            else {
-                treeDir.unshift(new ActivityTreeItem(childrenFileName, filePath, vscode.TreeItemCollapsibleState.Collapsed));
-            }
+    }
+  }
+  else if(filename=== getHelp() &&  fileParentPath===getDistinguishMark()){
+    treeDir.push(new ActivityTreeItem(getHardwaveDesc(), '', vscode.TreeItemCollapsibleState.None));
+    treeDir.push(new ActivityTreeItem(getApiDesc(), '', vscode.TreeItemCollapsibleState.None));
+  }
+  else {
+    let filePath: string;
+    if (path.dirname(activityPath) === fileParentPath) {
+      filePath = activityPath;
+    }
+    else {
+      filePath = path.join(fileParentPath, filename);
+    }
+    const files = fs.readdirSync(filePath);
+    // const activityPath: string = getPluginConfigActivityProject();
+    const appFile = getProjectConfigAppFile(activityPath);
+    if (appFile !== undefined) {
+      for (let i = 0; i < files.length; i++) {
+        const childrenFileName: string = files[i];
+        const childrenFilePath: string = path.join(filePath, childrenFileName);
+        const relativeFilePath: string = path.relative(activityPath, childrenFilePath);
+        if (appFile.indexOf(relativeFilePath) !== -1) {
+          if (fs.statSync(childrenFilePath).isFile()) {
+            treeDir.push(new ActivityTreeItem(childrenFileName, filePath, vscode.TreeItemCollapsibleState.None));
+          }
+          else {
+            treeDir.unshift(new ActivityTreeItem(childrenFileName, filePath, vscode.TreeItemCollapsibleState.Collapsed));
           }
         }
       }
-      else {
-        vscode.window.showErrorMessage('活动工程目录展开失败,插件appFile项获取异常', { modal: true });
-      }
-      return Promise.resolve(treeDir);
+    }
+    else {
+      vscode.window.showErrorMessage('活动工程目录展开失败,插件appFile项获取异常', { modal: true });
     }
   }
-
-  // private pathExists(p: string): boolean {
-  //   try {
-  //     fs.accessSync(p);
-  //   } catch (err) {
-  //     return false;
-  //   }
-  //   return true;
-  // }
+  return treeDir;
 }
 
 export class ActivityTreeItem extends vscode.TreeItem {
@@ -104,6 +138,7 @@ export class ActivityTreeItem extends vscode.TreeItem {
       this.parentPath
     ]
   };
+  iconPath:'C:\\Users\\AAA\\Downloads\\开启.svg' | undefined;
   tooltip = this.parentPath;
   contextValue = 'ActivityTreeItem';
 }
